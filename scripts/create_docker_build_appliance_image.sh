1a0f35654ab1870fca557d03365329713148d388
Ilya Dmitrichenko <errordeveloper@gmail.com>
2015-01-19 23:26:57 -0500

    Add script that creates the docker images of build appliance

---
+++ scripts/create_docker_build_appliance_image.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -x
+
+image_tarball="${1:-/home/vagrant/poky/build}/tmp/deploy/images/qemux86-64/container-build-appliance-qemux86-64.tar.bz2"
+docker_build_dir="$(realpath $(dirname $0)/../misc/docker/build_appliance/)" 
+
+cp ${image_tarball} ${docker_build_dir} \
+  && docker build -t yocto-build-appliance-x86_64 ${docker_build_dir}
