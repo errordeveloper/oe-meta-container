39eb0bc10c7862c0553dfa359143f8b6650772e8
Ilya Dmitrichenko <errordeveloper@gmail.com>
2015-01-20 00:29:12 -0500

    Few improvments on the docker side

--- scripts/create_docker_build_appliance_image.sh
+++ scripts/create_docker_build_appliance_image.sh
@@ -4,4 +4,4 @@ image_tarball="${1:-/home/vagrant/poky/build}/tmp/deploy/images/qemux86-64/conta
 docker_build_dir="$(realpath $(dirname $0)/../misc/docker/build_appliance/)" 
 
 cp ${image_tarball} ${docker_build_dir} \
-  && docker build -t yocto-build-appliance-x86_64 ${docker_build_dir}
+  && sudo docker build -t yocto-build-appliance-x86_64 ${docker_build_dir}
